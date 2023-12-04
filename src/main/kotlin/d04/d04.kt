4265d08f222ea63ba8129b37c12a19fa3a511917
Andreas Uschold <andreas.uschold@iits-consulting.de>
2023-12-04 10:20:42 +0100

    d04.2 solved

--- src/main/kotlin/d04/d04.kt
+++ src/main/kotlin/d04/d04.kt
@@ -6,28 +6,52 @@ import kotlin.math.pow
 fun main() {
     val file = File("src/main/resources/d04/input.txt")
 
-    val scratchCards = file.readLines()
-        .map { parseScratchCard(it) }
+    val game = Game(file.readLines().map { parseScratchCard(it) })
 
-    val checkSum = scratchCards.sumOf { it.points() }
-    println(checkSum)
+    val checkSum1 = game.scratchCards.sumOf { it.points() }
+    println(checkSum1)
+
+    game.scratchCards.forEachIndexed() { index, card ->
+        val matches = card.numberOfMatches()
+        val amountOfCard = game.ownedAmountOfCard(index)
+        game.addWonCards(index, matches, amountOfCard)
+    }
+
+    val checksum2 = game.scratchCards.mapIndexed {
+        index, _ -> game.ownedAmountOfCard(index)
+    }.sum()
+    println(checksum2)
+}
+
+private data class Game(
+    val scratchCards: List<ScratchCard>
+) {
+    private val virtualCards: MutableList<Int> by lazy { MutableList(scratchCards.size) { 0 } }
+
+    fun addWonCards(cardIndex: Int, numberOfWonCards: Int, amountOfCardsPerIndex: Int) {
+        IntRange(cardIndex + 1, cardIndex + numberOfWonCards).forEach {
+            if (it < virtualCards.size) {
+                virtualCards[it] = virtualCards[it] + amountOfCardsPerIndex
+            }
+        }
+    }
+
+    fun ownedAmountOfCard(cardIndex: Int) = 1 + virtualCards[cardIndex]
 }
 
 private data class ScratchCard (
     val winningNumbers: List<Int>,
     val numbersYouHave: List<Int>,
 ) {
-    fun points(): Int {
-        val numberOfMatches = winningNumbers.map { numbersYouHave.contains(it) }
-            .count { it }
-
-        return when (numberOfMatches) {
-            0 -> 0
-            1 -> 1
-            else -> 2.0.pow(numberOfMatches - 1).toInt()
-        }
-
+    fun points(): Int = when (val matches = numberOfMatches()) {
+        0 -> 0
+        1 -> 1
+        else -> 2.0.pow(matches - 1).toInt()
     }
+
+    fun numberOfMatches(): Int = winningNumbers
+        .map { numbersYouHave.contains(it) }
+        .count { it }
 }
 
 private fun parseScratchCard(line: String): ScratchCard {
