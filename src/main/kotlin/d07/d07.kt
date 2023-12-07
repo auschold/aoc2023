00135c51daa6ba303ebeb3231bbab772f1037d3d
Andreas Uschold <andreas.uschold@iits-consulting.de>
2023-12-07 16:51:23 +0100

    d07.2 solved

--- src/main/kotlin/d07/d07.kt
+++ src/main/kotlin/d07/d07.kt
@@ -3,20 +3,24 @@ import java.io.File
 fun main() {
     val file = File("src/main/resources/d07/input.txt")
 
-    val handsWithBids : List<HandWithBid> = file.readLines()
+    val handsWithBids= file.readLines()
         .map { it.split(" ") }
         .map { HandWithBid(parseHand(it[0]), it[1].toInt()) }
 
-    val sortedHandsWithBids = handsWithBids.sortedWith(handWithBidComparator)
-    val checksum = sortedHandsWithBids.mapIndexed { index, handWithBid ->
-        (index + 1) * handWithBid.bid
-    }.sum()
+    val checksum1 = handsWithBids.sortedWith(Comparator.comparing(HandWithBid::hand, handComparator))
+        .mapIndexed { index, handWithBid ->
+            (index + 1) * handWithBid.bid
+        }.sum()
+    println(checksum1)
 
-    println(checksum)
+    val checksum2 = handsWithBids.sortedWith(Comparator.comparing(HandWithBid::hand, handWithJokerComparator))
+        .mapIndexed { index, handWithBid ->
+            (index + 1) * handWithBid.bid
+        }.sum()
+    println(checksum2)
 }
 
-private fun parseHand(input: String): Hand =
-    Hand(input.map { Card.fromSymbol(it) })
+private fun parseHand(input: String): Hand = Hand(input.map { Card.fromSymbol(it) })
 
 private data class HandWithBid(
     val hand: Hand,
@@ -27,21 +31,50 @@ private data class Hand(
     val cards: List<Card>
 ){
     val type: HandType = HandType.entries.first { it.predicate.invoke(cards) }
+    val bestHandWithJoker by lazy { findBestHandUsingJoker(this) }
+
+    fun handString() = cards.map { card -> card.symbol }.joinToString("")
+
+    private fun findBestHandUsingJoker(hand: Hand): Hand {
+        val inputs = mutableListOf(hand.handString())
+        val possibleSubstitutions = mutableSetOf<String>()
+
+        while (inputs.isNotEmpty()) {
+            val next = inputs.removeLast()
+            if (next.contains("J")) {
+                Card.withoutJoker().forEach {
+                    inputs.add(next.replaceFirst('J', it.symbol))
+                }
+            }
+            possibleSubstitutions.add(next)
+        }
+
+        val result =  possibleSubstitutions.map { parseHand(it) }
+            .reduce { a, b ->
+                if (handComparator.compare(a, b) > 0) a else b
+            }
+        return result
+    }
 
     override fun toString(): String {
-        return cards.map { card -> card.symbol }.joinToString("") + " [${type.name}]"
+        return handString() + " [${type.name}]"
     }
 }
 
+
 private val handComparator: Comparator<Hand> = Comparator.comparing(Hand::type).thenComparator { a, b ->
     a.cards.mapIndexed { index, card -> card to b.cards[index] }
-        .map { it.first compareTo it.second }
+        .map { cardComparator.compare(it.first, it.second) }
         .firstOrNull { it != 0 }
         ?: 0
 }
 
-private val handWithBidComparator: Comparator<HandWithBid> = Comparator.comparing(HandWithBid::hand, handComparator)
-
+private val handWithJokerComparator: Comparator<Hand> = Comparator.comparing<Hand?, HandType?> { it.bestHandWithJoker.type }
+    .thenComparator { a, b -> a.cards.mapIndexed { index, card -> card to b.cards[index] }
+        .map { cardWithJokerComparator.compare(it.first, it.second) }
+        .firstOrNull { it != 0 }
+        ?: 0
+    }
 
 private enum class HandType(val predicate: (List<Card>) -> Boolean) {
     HIGH_CARD({ cards ->
@@ -73,7 +106,7 @@ private enum class HandType(val predicate: (List<Card>) -> Boolean) {
     }),
 }
 
-private enum class Card(val symbol: Char): Comparable<Card> {
+private enum class Card(val symbol: Char, val jokerOrdinal: Int? = null) {
     _2('2'),
     _3('3'),
     _4('4'),
@@ -83,7 +116,7 @@ private enum class Card(val symbol: Char): Comparable<Card> {
     _8('8'),
     _9('9'),
     T('T'),
-    J('J'),
+    J('J', -1),
     Q('Q'),
     K('K'),
     A('A');
@@ -91,5 +124,12 @@ private enum class Card(val symbol: Char): Comparable<Card> {
     companion object {
         fun fromSymbol(symbol: Char): Card =
             Card.entries.first { it.symbol == symbol }
+
+        fun withoutJoker(): List<Card> =
+            Card.entries.filter { it != J }
     }
 }
+
+private val cardComparator : Comparator<Card> = Comparator.comparing(Card::ordinal)
+
+private val cardWithJokerComparator : Comparator<Card> = Comparator.comparing { it.jokerOrdinal ?: it.ordinal }
