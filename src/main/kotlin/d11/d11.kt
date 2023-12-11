a372ce0a533fabf8c33ebe52c4b876f42fb8690e
Andreas Uschold <andreas.uschold@iits-consulting.de>
2023-12-11 16:08:06 +0100

    d11.2 solved

--- src/main/kotlin/d11/d11.kt
+++ src/main/kotlin/d11/d11.kt
@@ -10,10 +10,16 @@ fun main() {
     val checksum1 = universe.rawGalaxyCoordinates
         .flatMapIndexed { index: Int, first: Coordinate ->
             universe.rawGalaxyCoordinates.drop(index + 1).map { first to it }
-        }.map { universe.getDistanceBetween(it.first, it.second) }
-        .sum()
+        }.sumOf { universe.getDistanceBetween(it.first, it.second, expansionFactor = 2) }
 
     println(checksum1)
+
+    val checksum2 = universe.rawGalaxyCoordinates
+        .flatMapIndexed { index: Int, first: Coordinate ->
+            universe.rawGalaxyCoordinates.drop(index + 1).map { first to it }
+        }.sumOf { universe.getDistanceBetween(it.first, it.second, expansionFactor = 1_000_000) }
+
+    println(checksum2)
 }
 
 private fun parseUniverse(lines: List<String>): Universe =
@@ -29,9 +35,9 @@ private data class Universe(
     fun isRowEmpty(y: Int) = !rawGalaxyCoordinates.any { it.y == y }
     fun isColumnEmpty(x: Int) = !rawGalaxyCoordinates.any { it.x == x }
 
-    fun getDistanceBetween(a: Coordinate, b: Coordinate): Long {
-        val distX = IntRange.from(a.x, b.x).sumOf { if (isColumnEmpty(it)) 2L else 1L } - 1
-        val distY = IntRange.from(a.y, b.y).sumOf { if (isRowEmpty(it)) 2L else 1L } - 1
+    fun getDistanceBetween(a: Coordinate, b: Coordinate, expansionFactor: Long): Long {
+        val distX = IntRange.from(a.x, b.x).sumOf { if (isColumnEmpty(it)) expansionFactor else 1L } - 1
+        val distY = IntRange.from(a.y, b.y).sumOf { if (isRowEmpty(it)) expansionFactor else 1L } - 1
 
         return distX + distY
     }
